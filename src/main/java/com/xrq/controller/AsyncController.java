a0fec11b2f049c7becdd43991b6f470609d4119c
xiaoruiqing <xiaoruiqing@ehousechina.com>
2020-12-10 16:01:06 +0800

    @Async

---
+++ src/main/java/com/xrq/controller/AsyncController.java
@@ -0,0 +1,43 @@
+package com.xrq.controller;
+
+
+import com.xrq.component.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Future;
+
+@RestController
+public class AsyncController {
+
+
+    @Autowired
+    private Task task;
+
+
+    @GetMapping(value = "/asynctest")
+    public void test() throws Exception {
+
+        long start = System.currentTimeMillis();
+
+        Future<String> task1 = task.doTaskOne();
+        Future<String> task2 = task.doTaskTwo();
+        Future<String> task3 = task.doTaskThree();
+
+        while (true) {
+            if (task1.isDone() && task2.isDone() && task3.isDone()) {
+                // 三个任务都调用完成，退出循环等待
+                break;
+            }
+            Thread.sleep(1000);
+        }
+
+        long end = System.currentTimeMillis();
+
+        System.out.println("任务全部完成，总耗时：" + (end - start) + "毫秒");
+
+    }
+
+
+}
